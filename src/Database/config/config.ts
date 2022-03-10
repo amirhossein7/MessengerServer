78b7a584a220b8bdbce70c6c3ea63075ede38473
amirhossein <amirhosseineskini1@gmail.com>
2022-03-10 12:29:04 +0330

    update dbConfig production

--- src/Database/config/config.ts
+++ src/Database/config/config.ts
@@ -18,11 +18,12 @@ const dbConfig = {
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    "username": process.env.MYSQL_USERNAME,
+    "password": process.env.MYSQL_PASSWORD,
+    "database": process.env.MYSQL_DATABASE,
+    "host":     process.env.MYSQL_HOSTNAME,
+    "dialect":  process.env.MYSQL_DIALECT,
+    "logging": false
   }
 }
 
