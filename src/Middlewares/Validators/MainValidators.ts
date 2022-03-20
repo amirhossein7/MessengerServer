01b33dd08e0aae181cc3e647094127ab9b47c5cd
amirhossein <amirhosseineskini1@gmail.com>
2022-03-20 13:07:44 +0330

    implement jwt middleware

--- src/Middlewares/Validators/MainValidators.ts
+++ src/Middlewares/Validators/MainValidators.ts
@@ -1,5 +1,6 @@
 import {Request, Response, NextFunction} from 'express';
 import {validationResult} from 'express-validator';
+import JWT_handler from '../../Utils/Verification/JWT/Jwt';
 
 class ValidatorMiddleware {
 
@@ -9,6 +10,21 @@ class ValidatorMiddleware {
             return res.json(error);
         }
         next();
+    }
+
+    jwtValidation (req: Request, res: Response, next: NextFunction) {
+        const bearerHeader = req.headers['authorization'];
+        if(typeof bearerHeader !== 'undefined') {
+            JWT_handler.verify(bearerHeader).then((payload) => {
+                next(payload);
+            }).catch(_ => {
+                // Forbidden
+                res.sendStatus(403);
+            });
+        }else {
+            // Forbidden
+            res.sendStatus(403);
+        }
     }
 }
 
--- src/Utils/Verification/JWT/Jwt.ts
+++ src/Utils/Verification/JWT/Jwt.ts
@@ -1,6 +1,4 @@
 import * as jwt from 'jsonwebtoken';
-import {Request, Response, NextFunction} from 'express';
-
 import * as dotenv from 'dotenv';
 import { rejects } from 'assert';
 dotenv.config();
@@ -14,22 +12,6 @@ class JWT_handler {
     /// create token
     static sign(object: object, expiration: string = "30s"): string {
         return jwt.sign(object, secret_key, {expiresIn: expiration})
-    }
-
-    /// verify JWT middleware
-    static verifyMiddleware (req: Request, res: Response, next: NextFunction) {
-        const bearerHeader = req.headers['authorization'];
-        if(typeof bearerHeader !== 'undefined') {
-            this.verify(bearerHeader).then((payload) => {
-                next(payload);
-            }).catch(_ => {
-                // Forbidden
-                res.sendStatus(403);
-            });
-        }else {
-            // Forbidden
-            res.sendStatus(403);
-        }
     }
 
     /// verify JWT
